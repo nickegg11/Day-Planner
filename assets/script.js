95a7dc1c8fcef40f6780941a2812844f5976abe1
nickegg11 <nickeggleston@yahoo.com>
2023-11-06 14:31:27 -0500

    Current-day-displayed

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var today = dayjs().format('MMM D, YYYY');
+$('#currentDay').text(today);
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -9,7 +12,7 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
-    document.getElementById("saveBtn").addEventListener("click", displayDate);
+  
   
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
